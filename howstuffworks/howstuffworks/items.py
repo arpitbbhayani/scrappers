10abddbb2c800812377644c91cba6fef80040e9e
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2015-11-16 21:58:50 +0530

    Hello World of HowStuffWorks Spider

---
+++ howstuffworks/howstuffworks/items.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class HowstuffworksItem(scrapy.Item):
+    # define the fields for your item here like:
+    url = scrapy.Field()
+    title = scrapy.Field()
+    link = scrapy.Field()
+    desc = scrapy.Field()
+    excerpt = scrapy.Field()
+    image = scrapy.Field()
+    article_date = scrapy.Field()
